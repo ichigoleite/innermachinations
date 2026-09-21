8bc706c57cb7ec1ae7463917fdc0bcaab6a7b810
ichigoleite <picelboi@picelboi.xyz>
2026-09-20 21:18:41 -0400

    make content collection of images

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,27 @@
 // Import the glob loader
-import { glob } from "astro/loaders";
+import { file } from "astro/loaders";
 // Import utilities from `astro:content`
 import { defineCollection } from "astro:content";
 // Import Zod
-import { z } from "astro/zod";
 
 // Define a `loader` and `schema` for each collection
 const questions = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/questions" }),
-  schema: ({ image }) =>
-    z.object({
-      question: z.string(),
-      answer: z.string(),
-      to: z.string().default("N/A"),
-      date: z.date().default(new Date("2026-09-01")),
-      image: image(),
-    }),
+  loader: async () => {
+    const files = import.meta.glob<{ default: ImageMetadata }>(
+      `/src/content/questions/*.{jpeg,jpg,png,gif}`,
+      {
+        eager: true,
+      },
+    );
+    const items = Object.keys(files);
+    console.log(files);
+    // Must return an array of entries with an id property
+    // or an object with IDs as keys and entries as values
+    return items.map((item: any) => ({
+      id: item,
+      data: files[item],
+    }));
+  },
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = { questions };
